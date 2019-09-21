b06a35bf522c05187f824caa83a86190897e496f
Naomi Dushay <ndushay@stanford.edu>
2019-09-20 17:33:15 -0700

    add checksums method to objects

--- lib/preservation/client/objects.rb
+++ lib/preservation/client/objects.rb
@@ -11,6 +11,13 @@ module Preservation
         resp_body = get_json("objects/#{druid}.json", druid, 'current_version')
         resp_body[:current_version]
       end
+
+      # @param [Array] druids - required list of druids with or without prefix: 'druid:ab123cd4567' OR 'ab123cd4567'
+      # @param [String] :resp_format - desired format of the HTTP response (default csv, json also possible)
+      # @return body of HTTP response from Preservation API
+      def checksums(druids: [], resp_format: 'csv')
+        post('objects/checksums', { druids: druids, format: resp_format }, 'checksums')
+      end
     end
   end
 end
