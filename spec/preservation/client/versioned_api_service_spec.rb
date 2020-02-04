d62589841cb651f97bc052d496d5b35c190267c2
Michael J. Giarlo <mjgiarlo@stanford.edu>
2020-02-04 14:36:40 -0800

    Support patch, put, and delete methods
    Fixes #34
    
    Follows a suggestion by @jcoyne in #33.

--- spec/preservation/client/versioned_api_service_spec.rb
+++ spec/preservation/client/versioned_api_service_spec.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 RSpec.describe Preservation::Client::VersionedApiService do
-  let(:prez_api_url) { 'https://prezcat.example.com' }
   let(:auth_token) { 'my_secret_jwt_value' }
+  let(:conn) { Preservation::Client.instance.send(:connection) }
   let(:druid) { 'oo000oo0000' }
   let(:faraday_err_msg) { 'faraday is sad' }
+  let(:prez_api_url) { 'https://prezcat.example.com' }
+  let(:subject) { described_class.new(connection: conn, api_version: 'v6') }
 
   before do
     Preservation::Client.configure(url: prez_api_url, token: auth_token)
   end
 
-  let(:conn) { Preservation::Client.instance.send(:connection) }
-  let(:subject) { described_class.new(connection: conn, api_version: 'v6') }
-
   it 'populates api_version' do
     expect(subject.send(:api_version)).to eq 'v6'
   end
+
   it 'populates connection' do
     expect(subject.send(:connection)).to eq conn
   end
@@ -29,6 +29,7 @@ RSpec.describe Preservation::Client::VersionedApiService do
       stub_request(:get, "#{prez_api_url}/#{api_version}/#{path}").to_return(body: resp_body, status: 200)
       expect(subject.send(:get_json, path, druid)).to eq JSON.parse(resp_body)
     end
+
     it 'request url has no api_version when it is blank' do
       pc = described_class.new(connection: conn, api_version: '')
       resp_body = JSON.generate(bar: 'blank api version')
@@ -61,98 +62,109 @@ RSpec.describe Preservation::Client::VersionedApiService do
     end
   end
 
-  describe '#get' do
-    let(:path) { 'my_path' }
-    let(:params) { { foo: 'bar' } }
-    let(:params_as_args) { 'foo=bar' }
-    let(:api_version) { subject.send(:api_version) }
+  describe 'HTTP actions without bodies' do
+    %i[delete get].each do |method|
+      describe "##{method}" do
+        let(:api_version) { subject.send(:api_version) }
+        let(:params) { { foo: 'bar' } }
+        let(:params_as_args) { 'foo=bar' }
+        let(:path) { 'my_path' }
 
-    it 'request url includes api_version when it is non-blank' do
-      stub_request(:get, "#{prez_api_url}/#{api_version}/#{path}?#{params_as_args}")
-        .to_return(body: 'have api version', status: 200)
-      expect(subject.send(:get, path, params)).to eq 'have api version'
-    end
-    it 'request url has no api_version when it is blank' do
-      pc = described_class.new(connection: conn, api_version: '')
-      stub_request(:get, "#{prez_api_url}/#{path}?#{params_as_args}").to_return(body: 'blank api version', status: 200)
-      expect(pc.send(:get, path, params)).to eq 'blank api version'
-    end
+        it 'request url includes api_version when it is non-blank' do
+          stub_request(method, "#{prez_api_url}/#{api_version}/#{path}?#{params_as_args}")
+            .to_return(body: 'have api version', status: 200)
+          expect(subject.send(method, path, params)).to eq 'have api version'
+        end
 
-    context 'when response status success' do
-      let(:resp_body) { "I'm a little teacup" }
+        it 'request url has no api_version when it is blank' do
+          pc = described_class.new(connection: conn, api_version: '')
+          stub_request(method, "#{prez_api_url}/#{path}?#{params_as_args}").to_return(body: 'blank api version', status: 200)
+          expect(pc.send(method, path, params)).to eq 'blank api version'
+        end
 
-      it 'returns response body' do
-        stub_request(:get, "#{prez_api_url}/#{api_version}/#{path}?#{params_as_args}")
-          .to_return(body: resp_body, status: 200)
-        expect(subject.send(:get, path, params)).to eq resp_body
-      end
-    end
+        context 'when response status success' do
+          let(:resp_body) { "I'm a little teacup" }
 
-    context 'when response status 404' do
-      it 'raises Preservation::Client::NotFound with message from ResponseErrorFormatter' do
-        stub_request(:get, "#{prez_api_url}/#{api_version}/#{path}?#{params_as_args}").to_return(status: 404)
-        expect { subject.send(:get, path, params) }.to raise_error(Preservation::Client::NotFoundError, /got 404/)
-      end
-    end
+          it 'returns response body' do
+            stub_request(method, "#{prez_api_url}/#{api_version}/#{path}?#{params_as_args}")
+              .to_return(body: resp_body, status: 200)
+            expect(subject.send(method, path, params)).to eq resp_body
+          end
+        end
 
-    context 'when response status is an error' do
-      it 'raises Preservation::Client::UnexpectedResponseError with message from ResponseErrorFormatter' do
-        stub_request(:get, "#{prez_api_url}/#{api_version}/#{path}?#{params_as_args}").to_return(status: 500)
-        expect { subject.send(:get, path, params) }.to raise_error(Preservation::Client::UnexpectedResponseError, /got 500/)
-      end
-    end
+        context 'when response status 404' do
+          it 'raises Preservation::Client::NotFound with message from ResponseErrorFormatter' do
+            stub_request(method, "#{prez_api_url}/#{api_version}/#{path}?#{params_as_args}").to_return(status: 404)
+            expect { subject.send(method, path, params) }.to raise_error(Preservation::Client::NotFoundError, /got 404/)
+          end
+        end
 
-    context 'when response status is a redirect' do
-      it 'raises Preservation::Client::UnexpectedResponseError with message from ResponseErrorFormatter' do
-        stub_request(:get, "#{prez_api_url}/#{api_version}/#{path}?#{params_as_args}").to_return(status: 301)
-        expect { subject.send(:get, path, params) }.to raise_error(Preservation::Client::UnexpectedResponseError, /got 301/)
+        context 'when response status is an error' do
+          it 'raises Preservation::Client::UnexpectedResponseError with message from ResponseErrorFormatter' do
+            stub_request(method, "#{prez_api_url}/#{api_version}/#{path}?#{params_as_args}").to_return(status: 500)
+            expect { subject.send(method, path, params) }.to raise_error(Preservation::Client::UnexpectedResponseError, /got 500/)
+          end
+        end
+
+        context 'when response status is a redirect' do
+          it 'raises Preservation::Client::UnexpectedResponseError with message from ResponseErrorFormatter' do
+            stub_request(method, "#{prez_api_url}/#{api_version}/#{path}?#{params_as_args}").to_return(status: 301)
+            expect { subject.send(method, path, params) }.to raise_error(Preservation::Client::UnexpectedResponseError, /got 301/)
+          end
+        end
       end
     end
   end
 
-  describe '#post' do
-    let(:path) { 'my_path' }
-    let(:druids) { ['oo000oo0000', 'oo111oo1111'] }
-    let(:params) { { druids: druids } }
-    let(:api_version) { subject.send(:api_version) }
+  describe 'HTTP actions with bodies' do
+    %i[patch post put].each do |method|
+      describe "##{method}" do
+        let(:api_version) { subject.send(:api_version) }
+        let(:druids) { ['oo000oo0000', 'oo111oo1111'] }
+        let(:params) { { druids: druids } }
+        let(:path) { 'my_path' }
 
-    it 'request url includes api_version when it is non-blank' do
-      stub_request(:post, "#{prez_api_url}/#{api_version}/#{path}").to_return(body: 'have api version', status: 200)
-      expect(subject.send(:post, path, params)).to eq 'have api version'
-    end
-    it 'request url has no api_version when it is blank' do
-      pc = described_class.new(connection: conn, api_version: '')
-      stub_request(:post, "#{prez_api_url}/#{path}").to_return(body: 'blank api version', status: 200)
-      expect(pc.send(:post, path, params)).to eq 'blank api version'
-    end
-    it 'request sends params in body and content-type in header' do
-      pc = described_class.new(connection: conn, api_version: '')
-      stub_request(:post, "#{prez_api_url}/#{path}")
-        .with(body: params.to_json, headers: { 'Content-Type' => 'application/json' })
-        .to_return(body: 'blank api version', status: 200)
-      expect(pc.send(:post, path, params)).to eq 'blank api version'
-    end
+        it 'request url includes api_version when it is non-blank' do
+          stub_request(method, "#{prez_api_url}/#{api_version}/#{path}").to_return(body: 'have api version', status: 200)
+          expect(subject.send(method, path, params)).to eq 'have api version'
+        end
 
-    context 'when response status success' do
-      let(:resp_body) { JSON.generate(foo: 'bar') }
+        it 'request url has no api_version when it is blank' do
+          pc = described_class.new(connection: conn, api_version: '')
+          stub_request(method, "#{prez_api_url}/#{path}").to_return(body: 'blank api version', status: 200)
+          expect(pc.send(method, path, params)).to eq 'blank api version'
+        end
 
-      it 'returns response body' do
-        stub_request(:post, "#{prez_api_url}/#{api_version}/#{path}").to_return(body: resp_body, status: 200)
-        expect(subject.send(:post, path, params)).to eq resp_body
-      end
-    end
+        it 'request sends params in body and content-type in header' do
+          pc = described_class.new(connection: conn, api_version: '')
+          stub_request(method, "#{prez_api_url}/#{path}")
+            .with(body: params.to_json, headers: { 'Content-Type' => 'application/json' })
+            .to_return(body: 'blank api version', status: 200)
+          expect(pc.send(method, path, params)).to eq 'blank api version'
+        end
 
-    context 'when response status 404' do
-      it 'raises Preservation::Client::NotFound with message from ResponseErrorFormatter' do
-        stub_request(:post, "#{prez_api_url}/#{api_version}/#{path}").to_return(status: 404)
-        expect { subject.send(:post, path, params) }.to raise_error(Preservation::Client::NotFoundError, /got 404/)
-      end
-    end
+        context 'when response status is 200' do
+          let(:resp_body) { JSON.generate(foo: 'bar') }
 
-    context 'when response status NOT success or 404' do
-      it 'raises Preservation::Client::UnexpectedResponseError with message from ResponseErrorFormatter' do
-        stub_request(:post, "#{prez_api_url}/#{api_version}/#{path}").to_return(status: 500)
-        expect { subject.send(:post, path, params) }.to raise_error(Preservation::Client::UnexpectedResponseError, /got 500/)
+          it 'returns response body' do
+            stub_request(method, "#{prez_api_url}/#{api_version}/#{path}").to_return(body: resp_body, status: 200)
+            expect(subject.send(method, path, params)).to eq resp_body
+          end
+        end
+
+        context 'when response status is 404' do
+          it 'raises Preservation::Client::NotFound with message from ResponseErrorFormatter' do
+            stub_request(method, "#{prez_api_url}/#{api_version}/#{path}").to_return(status: 404)
+            expect { subject.send(method, path, params) }.to raise_error(Preservation::Client::NotFoundError, /got 404/)
+          end
+        end
+
+        context 'when response status is neither 200 or 404' do
+          it 'raises Preservation::Client::UnexpectedResponseError with message from ResponseErrorFormatter' do
+            stub_request(method, "#{prez_api_url}/#{api_version}/#{path}").to_return(status: 500)
+            expect { subject.send(method, path, params) }.to raise_error(Preservation::Client::UnexpectedResponseError, /got 500/)
+          end
+        end
       end
     end
   end
